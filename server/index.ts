782a86bcc7130ad147045bb6014e785f0ff7a3f3
Prav-learnbay <pravs.0606@gmail.com>
2025-04-08 23:55:49 +0530

    initial commit

--- server/index.ts
+++ server/index.ts
@@ -73,12 +73,8 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port} at 0.0.0.0`);
+  server.listen(port, () => {
+    log(`serving on port ${port}`);
     log(`Server is ready to accept connections`);
   });
 })();
